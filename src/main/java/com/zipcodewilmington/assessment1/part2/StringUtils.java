0c52b00a899d1f7e15a46f0228fece471670fca4
Arcelia Calleja <arceliacalleja@gmail.com>
2019-02-24 19:08:01 -0500

    Finish my part2 exam

--- src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
+import java.util.ArrayList;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -11,7 +15,24 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return an array of strings, each element representative of a respective word in the sentence
      */
     public static String[] getWords(String sentence) {
-        return null;
+
+        ArrayList<String> arrayListPalabras = new ArrayList<>();
+        String palabra = "";
+        String letra="";
+        for (int i = 0; i < sentence.length(); i++) {
+            letra=Character.toString(sentence.charAt(i));
+            if (!letra.contains(" ")){  //condicion si no es un espacio
+                //voy a agregar la letra a la palabra
+                palabra= palabra  + letra;
+            }else {
+                //Ya tengo una palabra completa
+                arrayListPalabras.add(palabra);
+                palabra = "";
+            };
+        }
+        arrayListPalabras.add(palabra);
+        String[] newarray=arrayListPalabras.toArray(new String[0]);
+        return newarray;
     }
 
 
@@ -21,7 +42,8 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word of the sentence
      */
     public static String getFirstWord(String sentence) {
-        return null;
+
+        return getWords(sentence)[0];
     }
 
     /**
@@ -30,7 +52,12 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order
      */
     public static String reverseFirstWord(String sentence) {
-        return null;
+
+
+        String reverse=BasicStringUtils.reverse(getFirstWord(sentence));
+
+        return reverse;
+
     }
 
     /**
@@ -39,7 +66,10 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order with the first character capitalized
      */
     public static String reverseFirstWordThenCamelCase(String sentence) {
-        return null;
+
+        return BasicStringUtils.camelCase(reverseFirstWord(sentence));
+
+
     }
 
 
@@ -50,7 +80,12 @@ public class StringUtils {
      * given a string and index, return an identical string excluding the character at the specified index
      */
     public static String removeCharacterAtIndex(String str, int index) {
-        return null;
+
+        String returnstr=new String();
+
+        returnstr=str.substring(0,index) + str.substring(index +1, str.length());
+
+        return returnstr;
     }
 
 }
